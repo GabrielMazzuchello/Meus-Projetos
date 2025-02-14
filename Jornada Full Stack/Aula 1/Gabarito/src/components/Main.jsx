be8c5f76a1f7aefe650c9160dffa964812dfed29
Gabriel Mazzuchello Dal Molin <94196035+GabrielMazzuchello@users.noreply.github.com>
2025-02-14 17:26:07 -0300

    todos os arquivos

---
+++ Jornada Full Stack/Aula 1/Gabarito/src/components/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div className='main'>
+      <div className='item-list__header'>
+        <div className='main__text'>
+          <h2>Artistas populares</h2>
+          <a className='item-list__link' href="/">Mostrar tudo</a>
+          </div>
+      </div>
+    </div>
+  )
+}
+
+export default Main
